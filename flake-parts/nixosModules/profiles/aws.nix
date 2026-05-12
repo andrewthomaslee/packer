f48164184ad3c693483b36a46b457736c810395e
andrewthomaslee <andrewthomaslee.business@gmail.com>
2026-05-12 00:38:25 -0500

    profiles

--- flake-parts/nixosModules/profiles/aws.nix
+++ flake-parts/nixosModules/profiles/aws.nix
@@ -1,10 +1,9 @@
 {...}: {
-  flake.nixosModules.aws = {lib, ...}: {
+  flake.nixosModules.aws = {...}: {
     disko.devices.disk.main.device = "/dev/nvme0n1";
 
-    boot.initrd.availableKernelModules = ["ahci" "xhci_pci" "virtio_pci" "virtio_scsi" "sd_mod" "sr_mod" "nvme"];
+    boot.initrd.availableKernelModules = ["nvme"];
 
-    networking.useDHCP = true;
-    networking.interfaces.ens5.useDHCP = lib.mkDefault true;
+    networking.interfaces.ens5.useDHCP = true;
   };
 }
--- flake-parts/nixosModules/profiles/hcloud.nix
+++ flake-parts/nixosModules/profiles/hcloud.nix
@@ -4,9 +4,6 @@
 
     boot.initrd.availableKernelModules = ["ahci" "xhci_pci" "virtio_pci" "virtio_scsi" "sd_mod" "sr_mod"];
 
-    networking.interfaces = {
-      enp1s0.useDHCP = true;
-      enp7s0.useDHCP = true;
-    };
+    networking.interfaces.enp1s0.useDHCP = true;
   };
 }
