0962be1d9b9461357cedb4a3bff7b443e843eecc
andrewthomaslee <andrewthomaslee.business@gmail.com>
2026-05-11 23:21:29 -0500

    toplevel

--- flake-parts/packages/toplevel.nix
+++ flake-parts/packages/toplevel.nix
@@ -3,15 +3,15 @@
   perSystem = {...}: {
     packages = {
       # Hcloud
-      hcloud-zfs-x86_64 = self.nixosConfigurations.hcloud-x86_64.config.system.build.toplevel;
-      hcloud-zfs-aarch64 = self.nixosConfigurations.hcloud-aarch64.config.system.build.toplevel;
-      hcloud-ext4-x86_64 = self.nixosConfigurations.hcloud-x86_64.config.system.build.toplevel;
-      hcloud-ext4-aarch64 = self.nixosConfigurations.hcloud-aarch64.config.system.build.toplevel;
+      hcloud-zfs-x86_64 = self.nixosConfigurations.hcloud-zfs-x86_64.config.system.build.toplevel;
+      hcloud-zfs-aarch64 = self.nixosConfigurations.hcloud-zfs-aarch64.config.system.build.toplevel;
+      hcloud-ext4-x86_64 = self.nixosConfigurations.hcloud-ext4-x86_64.config.system.build.toplevel;
+      hcloud-ext4-aarch64 = self.nixosConfigurations.hcloud-ext4-aarch64.config.system.build.toplevel;
       # AWS
-      aws-zfs-x86_64 = self.nixosConfigurations.aws-x86_64.config.system.build.toplevel;
-      aws-zfs-aarch64 = self.nixosConfigurations.aws-aarch64.config.system.build.toplevel;
-      aws-ext4-x86_64 = self.nixosConfigurations.aws-x86_64.config.system.build.toplevel;
-      aws-ext4-aarch64 = self.nixosConfigurations.aws-aarch64.config.system.build.toplevel;
+      aws-zfs-x86_64 = self.nixosConfigurations.aws-zfs-x86_64.config.system.build.toplevel;
+      aws-zfs-aarch64 = self.nixosConfigurations.aws-zfs-aarch64.config.system.build.toplevel;
+      aws-ext4-x86_64 = self.nixosConfigurations.aws-ext4-x86_64.config.system.build.toplevel;
+      aws-ext4-aarch64 = self.nixosConfigurations.aws-ext4-aarch64.config.system.build.toplevel;
     };
   };
 }
